10b250976f25fb27d2a4da15b78b63e513491cd8
sambhav sharma <118333908+SambhavSharmaz@users.noreply.github.com>
2025-07-02 13:24:03 +0530

    update

---
+++ Day12/part1_proj/src/components/Categories.jsx
@@ -0,0 +1,14 @@
+const Categories = () => {
+    return (
+        <div>
+            <h1>Categories</h1>
+            <ul>
+                <li>Category 1</li>
+                <li>Category 2</li>
+                <li>Category 3</li>
+            </ul>
+        </div>
+    )
+}
+
+export {Categories};
